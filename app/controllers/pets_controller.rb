ded40e312f417eb21acdb6c0c7d8d4e457ab9912
David Ingram <david@ingramaviation.com>
2019-11-20 13:54:16 -0600

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,56 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  # post '/pets' do
+  #   if params[:pet][:owner_ids]
+  #     @pet = Pet.create(name: params[:pet_name], owner_id: params[:pet][:owner_ids].last)
+  #   elsif !params[:owner][:name].empty?
+  #     @owner = Owner.create(name: params[:owner][:name])
+  #     @pet = Pet.create(name: params[:pet_name], owner_id: @owner)
+  #     binding.pry
+  #   end
+  #     #binding.pry
+  #   redirect "pets/#{@pet.id}"
+  # end
 
-    redirect to "pets/#{@pet.id}"
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+      if !params[:owner][:name].empty?
+        @pet.owner = Owner.create(name: params[:owner][:name])
+      end
+      # if !params[:pet][:owner_ids].empty?
+      #   @pet.owner = Owner.find_by(params[:pet][:owner_ids].first.to_i)
+      # end
+    @pet.save
+    #binding.pry
+    redirect "pets/#{@pet.id}"
   end
-
-  get '/pets/:id' do 
+  
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
 
-    redirect to "pets/#{@pet.id}"
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
   end
 end
